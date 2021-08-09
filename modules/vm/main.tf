69a4c5cba994de1505a5229c7af6fd73ac72d451
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-09 12:59:15 +0200

    feat(modules/vm): Create module for VM

---
+++ modules/vm/main.tf
@@ -0,0 +1,42 @@
+resource "azurerm_network_interface" "main" {
+  location            = var.resource_group.location
+  resource_group_name = var.resource_group.name
+
+  name = "example"
+
+  ip_configuration {
+    name                          = "dafault"
+    subnet_id                     = var.subnet.id
+    public_ip_address_id          = var.public_ip.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  resource_group_name = var.resource_group.name
+  location            = var.resource_group.location
+
+  name           = var.name
+  size           = var.size
+  admin_username = "default"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  admin_ssh_key {
+    username   = "default"
+    public_key = var.ssh_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
---
+++ modules/vm/variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group" {
+  description = "Resource Group Terraform object"
+}
+
+variable "public_ip" {
+  description = "Public IP Terraform object"
+}
+
+variable "subnet" {
+  description = "Network Subnet Terraform object"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of virtual machine"
+}
+
+variable "size" {
+  type        = string
+  description = "Size of VM. Available sizes: https://docs.microsoft.com/en-us/azure/cloud-services/cloud-services-sizes-specs"
+  default     = "Standard_A1_v2"
+}
+
+variable "ssh_key" {
+  type        = string
+  description = "Admin user SSH key"
+}
