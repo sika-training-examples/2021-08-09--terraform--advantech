cbdc675a43598c1bf4ed8464f83efc5833691050
Ondrej Sika <ondrej@ondrejsika.com>
2021-08-09 10:46:01 +0200

    feat: Create network & subnet for VMs

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ resource "azurerm_resource_group" "default" {
   name     = "default"
   location = "westeurope"
 }
+
+resource "azurerm_virtual_network" "default" {
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  name          = "default-network"
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "vms" {
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+
+  name             = "vms"
+  address_prefixes = ["10.0.0.0/24"]
+}
